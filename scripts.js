70a2298742219782156c5c07545e99bfd3f34dc6
Asmita Mandal <156453146+asmxtaa@users.noreply.github.com>
2024-06-21 01:15:35 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchMarsWeather();
+    fetchAsteroids();
+    fetchGallery();
+});
+
+async function fetchMarsWeather() {
+}
+
+function displayMarsWeather(data) {
+}
+
+async function fetchAsteroids() {
+}
+
+function displayAsteroids(data) {
+}
+
+async function fetchGallery() {
+}
+
+function displayGallery(data) {
+}
